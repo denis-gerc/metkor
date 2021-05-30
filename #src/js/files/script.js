be98b082fd2624cbeddf30378454c0de2361a75b
denisgerc <denisgerc178@gmail.com>
2021-05-30 17:34:10 +0300

    completed progress

---
+++ #src/js/files/script.js
@@ -0,0 +1,26 @@
+// Counter //================================================
+window.onscroll = function () {
+	let windowPos = window.scrollY;
+	let counterPos = document.querySelector('.counter').offsetTop;
+	let scrollTrigger = counterPos - window.innerHeight + 200;
+	if (windowPos >= scrollTrigger) {
+		const counters = document.querySelectorAll('.counter__number');
+		const speed = 551; // The lower the slower
+		​
+		counters.forEach(counter => {
+			const updateCount = () => {
+				const target = +counter.getAttribute('data-target');
+				const count = +counter.innerText;
+				const inc = target / speed;​
+				// Check if target is reached
+				if (count < target) {
+					counter.innerText = Math.ceil(count + inc);
+					setTimeout(updateCount, 100);
+				} else {
+					counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+				}
+			};​
+			updateCount();
+		});
+	}
+}
--- metkor/js/app.js
+++ metkor/js/app.js
@@ -124,7 +124,32 @@ let slider_about = new Swiper('.portfolio-slider', {
 	//	el: '.swiper-scrollbar',
 	//},
 });
-
+// Counter //================================================
+window.onscroll = function () {
+	let windowPos = window.scrollY;
+	let counterPos = document.querySelector('.counter').offsetTop;
+	let scrollTrigger = counterPos - window.innerHeight + 200;
+	if (windowPos >= scrollTrigger) {
+		const counters = document.querySelectorAll('.counter__number');
+		const speed = 551; // The lower the slower
+		​
+		counters.forEach(counter => {
+			const updateCount = () => {
+				const target = +counter.getAttribute('data-target');
+				const count = +counter.innerText;
+				const inc = target / speed;​
+				// Check if target is reached
+				if (count < target) {
+					counter.innerText = Math.ceil(count + inc);
+					setTimeout(updateCount, 100);
+				} else {
+					counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+				}
+			};​
+			updateCount();
+		});
+	}
+}
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf("MSIE ");
 var isMobile = {
